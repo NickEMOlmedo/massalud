0736f7a28f014fc5332fbaae5d0ae425dd8b2490
GustavoAcosta84 <acgtavo@gmail.com>
2023-10-03 15:28:12 -0300

    Clase conexión

--- src/controlador/Conexion.java
+++ src/controlador/Conexion.java
@@ -1,10 +1,48 @@
 
 package controlador;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Nickolas
  */
 public class Conexion {
+     private static final String URL = "jdbc:mariadb://localhost:3306/massalud";
+    private static final String USUARIO = "root";
+    private static final String PASS = "";
+
+    private static Conexion conexion = null;
+    
+     private Conexion() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "¡Error al cargar el Driver de Conexion!");
+        }
+
+    }
+     
+      public static Connection getConexion() {
+        Connection con = null;
+        if (conexion == null) {
+
+            conexion = new Conexion();
+
+        }
+        try {
+            con = DriverManager.getConnection(URL + "?useLegacyDatetimeCode=false&serverTimezone=UTC" + "&user=" + USUARIO + "&password=" + PASS);
+
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "Error de conexion.");
+
+        }
+        return con;
+
+    }
     
 }
