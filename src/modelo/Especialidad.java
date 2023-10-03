d09ed1aacb3050f49e88aab382ccd171b2a6de72
GustavoAcosta84 <acgtavo@gmail.com>
2023-10-03 16:16:05 -0300

    Modelo Especialidad

--- src/modelo/Especialidad.java
+++ src/modelo/Especialidad.java
@@ -1,10 +1,44 @@
 
 package modelo;
 
-/**
- *
- * @author Nickolas
- */
+
 public class Especialidad {
+    
+    private int idEspecialidad;
+    private String especialidad;
+
+    public Especialidad() {
+    }
+
+    public Especialidad(int idEspecialidad, String especialidad) {
+        this.idEspecialidad = idEspecialidad;
+        this.especialidad = especialidad;
+    }
+
+    public Especialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public int getIdEspecialidad() {
+        return idEspecialidad;
+    }
+
+    public void setIdEspecialidad(int idEspecialidad) {
+        this.idEspecialidad = idEspecialidad;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Especialidad{" + "idEspecialidad=" + idEspecialidad + ", especialidad=" + especialidad + '}';
+    }
+    
     
 }
