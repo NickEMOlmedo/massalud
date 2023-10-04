0cb55b7cd72aa4d29c0e72ccf03541ab71a47d57
Fabiana76 <fabianamichis@gmail.com>
2023-10-04 17:14:14 -0300

    Prestador

--- src/controlador/PrestadorData.java
+++ src/controlador/PrestadorData.java
@@ -1,10 +1,32 @@
 
 package controlador;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Nickolas
  */
 public class PrestadorData {
+   
+    private Connection nuevaConexion = null;
     
-}
+    static boolean conexionExitosa;
+    
+    public PrestadorData(){
+        
+        nuevaConexion= Conexion.getConexion();
+        
+        if (nuevaConexion != null) {
+
+            conexionExitosa = true;
+    
+    }
+    }
+    public static boolean conexionExitosa(){
+        return conexionExitosa;
+    }
+    
+    
+    }
+
